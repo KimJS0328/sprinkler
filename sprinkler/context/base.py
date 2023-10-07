13ee14886425730d4db352e8365a27ba80816fd1
youngseok-yoo <youngseok.yoo@navercorp.com>
2023-10-07 05:55:01 +0000

    feature: context get arguments with unpacking output

--- sprinkler/context/base.py
+++ sprinkler/context/base.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
-from typing import Any
+from enum import Enum
+
+from typing import Dict, Tuple, Any
 
 
 class Context:
@@ -13,8 +15,8 @@ class Context:
     """
 
     output: Any
-    global_context: dict
-    history_context: dict
+    global_context: Dict
+    history_context: Dict
     
     def __init__(self) -> None:
         """Initializes all contexts to empty dictionary"""
@@ -22,39 +24,55 @@ class Context:
         self.global_context = {} 
         self.history_context = {}
 
-    def get_values(self, query: dict[str, Any]) -> dict[str, Any]:
-        """Retrieve values in context requested by query
+    def get_args(self, query: dict[str, Any]) -> Tuple[Tuple, Dict[str, Any]]:
+        """Retrieve arguments in context requested by query
 
         result doesn't include value which doesn't exist in context
-        priority: history(if task_id is specifed) -> local -> previous output -> global
+        priority: history(if task_id is specifed) -> output -> global
         
         Args:
             query: key-value pair with {argument_name}: {source}
 
         Returns:
-            A dict mapping argument name to the correspoding value in the context
-            {argument_name}: {value}
+            Tuple of arguments (tuple) and keyword argument (dictionary)
         """
-        result = {}
+        args = ()
+        kwargs = {}
+
+        # validation of output type
+        output_mapping = False # flag for output mapping to kwargs
+
+        if self.output is None:
+            pass
+        elif (all(hasattr(self.output, attr) # output is a mapping object
+                for attr in ('keys', '__getitem__'))):
+            output_mapping = True
+        elif (all(hasattr(self.output, attr)
+                for attr in ('__iter__', '__next__')) and # output is a iterable object
+              not isinstance(self.output, str)):
+            args = self.output
+        else: # output is a independent object
+            arg = (self.output)
 
         for arg, src in query.items():
             src_parts = src.split(sep='.', maxsplit=2)
-            value = None
-
+            
+            # match argument with history context
             if src_parts == 2: 
-                task_id, arg = src_parts
-                if task_id in self.history_context:
-                    value = self.history_context[task_id].get(arg) 
+                task_id, task_src = src_parts
+                if (task_id in self.history_context and 
+                    task_src in self.history_context[task_id]):
+                    kwargs[arg] = self.history_context[task_id][task_src]
             else:
-                value = (
-                    self.local_context.get(src) 
-                    or self.output_context.get(src) 
-                    or self.global_context.get(src)
-                )
+                # match argument with global context
+                if src in self.global_context:
+                    kwargs[arg] = self.global_context[src]
 
-            if value: result[arg] = value
+                # match argument with output if mapping
+                if output_mapping and src in self.output.keys():
+                    kwargs[arg] = self.output.__getitem__(src)
 
-        return result
+        return args, kwargs
 
     
     def add_global(self, context: dict[str, Any]) -> None:
