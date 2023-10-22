79bec28e9d6975b31925d8020b8328c2c40967d6
KimJS0328 <wjddkel328@gmail.com>
2023-10-22 08:16:30 +0000

    test: add multiprocess test

--- tests/test_group.py
+++ tests/test_group.py
@@ -19,13 +19,13 @@ def test_group_with_context():
         )
     )
 
-    result = group.run_with_context(
+    output = group.run_with_context(
         {'repeat': 3},
         pipeline1='sprinkler',
         pipeline2=[1,2,3]
     )
 
-    assert result == {
+    assert output == {
         'pipeline1': 'sprinklersprinklersprinkler',
         'pipeline2': [1, 2, 3, 1, 2, 3, 1, 2, 3]
     }
@@ -49,13 +49,13 @@ def test_group_with_tuple_input():
         )
     )
 
-    result = group.run_with_context(
+    output = group.run_with_context(
         {'repeat': 3},
         p2=(3,4),
         __default__='sprinkler'
     )
 
-    assert result == {
+    assert output == {
         'p1': 'sprinklersprinklersprinkler',
         'p2': 7
     }
@@ -75,12 +75,12 @@ def test_group_with_no_input():
         Pipeline('p2').add(return_string)
     )
 
-    result = group.run_with_context(
+    output = group.run_with_context(
         {'repeat': 3},
         p1='sprinkler'
     )
 
-    assert result == {
+    assert output == {
         'p1': 'sprinklersprinklersprinkler',
         'p2': 'sprinkler'
     }
@@ -100,12 +100,40 @@ def test_group_with_default_arguments():
         Pipeline('p2').add(repeat_array)
     )
 
-    result = group.run(
+    output = group.run(
         p1=('sprinkler',),
         p2=([1,2,3],)
     )
 
-    assert result == {
+    assert output == {
         'p1': 'sprinklersprinklersprinkler',
         'p2': [1, 2, 3, 1, 2, 3, 1, 2, 3]
+    }
+
+
+def repeat_string(string: str, repeat: int = 3) -> str:
+    return string * repeat
+def repeat_array(array: list, repeat: int = 3) -> list:
+    return array * repeat
+
+def test_group_with_processpool():
+
+    pipeline1 = Pipeline('pipeline1')
+    pipeline1.add(Task('repeat_string', repeat_string))
+
+    pipeline2 = Pipeline('pipeline2')
+    pipeline2.add(Task('repeat_array', repeat_array))
+
+    group = Group('group', executor_type='process', max_workers=2).add(
+        pipeline1, pipeline2
+    )
+
+    output = group.run(
+        pipeline1=('sprinkler',),
+        pipeline2=([1,2,3],)
+    )
+
+    assert output == {
+        'pipeline1': 'sprinklersprinklersprinkler',
+        'pipeline2': [1, 2, 3, 1, 2, 3, 1, 2, 3]
     }
